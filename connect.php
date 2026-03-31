dd139f297c80e89086bd3a422a095ec447cb353c
Haiden Murphy <murphyhaiden@gmail.com>
2026-03-30 19:24:50 -0700

    Update connect.php
    added connection message in the else

--- connect.php
+++ connect.php
@@ -2,6 +2,9 @@
 $conn = new mysqli("localhost", "root", "root", "Rock Climbing Customer Management");
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+ die("Connection failed: " . $conn->connect_error);
+}
+else {
+ echo "Connection Succesful!";
 }
 ?>
